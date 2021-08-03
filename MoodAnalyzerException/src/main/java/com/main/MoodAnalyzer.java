ffabf64786a6cd978e478ff8991de83eb158c881
pratap1804 <pratap.1804@gmail.com>
2021-08-04 00:05:22 +0530

    Tested: both sad and happy mood messages

--- MoodAnalyzerException/src/main/java/com/main/MoodAnalyzer.java
+++ MoodAnalyzerException/src/main/java/com/main/MoodAnalyzer.java
@@ -2,12 +2,24 @@ package com.main;
 
 public class MoodAnalyzer {
 
-    public String analyzeMood(String message){
-        if(message.contains("sad")) {
+    String message;
+
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+    public MoodAnalyzer() {
+        super();
+    }
+    //Check mood analyzer method
+    public String analyzeMood(String message) {
+        if (message.contains("sad")) {
             return "SAD";
-        }
-        else{
+        } else {
             return "HAPPY";
         }
+    }
+    //Generated main method displaying welcome message
+    public static void main(String[] args) {
+        System.out.println("Welcome to the mood analyzer exception handing!");
     }
 }
--- MoodAnalyzerException/src/test/java/com/test/MoodAnalyzerTest.java
+++ MoodAnalyzerException/src/test/java/com/test/MoodAnalyzerTest.java
@@ -1,13 +1,20 @@
 package com.test;
 import com.main.MoodAnalyzer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class MoodAnalyzerTest {
 
-    //Generated main method displaying welcome message
-    public static void main(String[] args){
-        System.out.println("Welcome to the mood analyzer exception handing!");
-
+    @Test
+    void whenMessageContains_Sad_ShouldReturnSad(){
         MoodAnalyzer myCurrentMood = new MoodAnalyzer();
-        myCurrentMood.analyzeMood("i am sad");
+        Assertions.assertEquals("SAD", myCurrentMood.analyzeMood("i am in sad mood"));
+    }
+
+    @Test
+    void whenMessageContains_Happy_ShouldReturnHappy(){
+        MoodAnalyzer myCurrentMood = new MoodAnalyzer();
+        Assertions.assertEquals("HAPPY", myCurrentMood.analyzeMood("i am any mood"));
     }
 }
+
